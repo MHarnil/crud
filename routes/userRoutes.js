4d5b958a2afc34b638bb6b25c1a074e105966b49
MHarnil <mharnil87@gmail.com>
2025-02-17 14:46:40 +0530

    first commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,8 +17,7 @@ const router = express.Router();
 
 router.post('/users', async (req, res) => {
     try {
-        const user = new User(req.body);
-        await user.save();
+        const user = User.create(req.body);
         res.status(201).json(user);
     } catch (error) {
         res.status(400).json(error);
